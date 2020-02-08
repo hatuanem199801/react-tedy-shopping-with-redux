d02d83c6d4da23f45af07b8fb500e14ef300546a
Tuấn Hà <hatuanem199801@gmail.com>
2020-02-08 11:34:35 +0700

    Updates

---
+++ precache-manifest.936d136787361ef2b8ff60ee7299a713.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15da137e09cded54ab3815a85471e4d9",
+    "url": "/react-tedy-shopping-with-redux/index.html"
+  },
+  {
+    "revision": "fa0699e2f7a57fbf4fe9",
+    "url": "/react-tedy-shopping-with-redux/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5128cf65f50f6704edb7",
+    "url": "/react-tedy-shopping-with-redux/static/css/main.01eb59e9.chunk.css"
+  },
+  {
+    "revision": "fa0699e2f7a57fbf4fe9",
+    "url": "/react-tedy-shopping-with-redux/static/js/2.c765875e.chunk.js"
+  },
+  {
+    "revision": "5128cf65f50f6704edb7",
+    "url": "/react-tedy-shopping-with-redux/static/js/main.a972d5cd.chunk.js"
+  },
+  {
+    "revision": "39e7f6a61a279344e373",
+    "url": "/react-tedy-shopping-with-redux/static/js/runtime-main.4d04f3f7.js"
+  },
+  {
+    "revision": "69cec1ece498571869959ebce079fcb1",
+    "url": "/react-tedy-shopping-with-redux/static/media/apple-drinks.69cec1ec.jpg"
+  },
+  {
+    "revision": "9e592edb59e1e8c13f3a3ffb662661a1",
+    "url": "/react-tedy-shopping-with-redux/static/media/icecream.9e592edb.jpg"
+  },
+  {
+    "revision": "9e8968b69a96bd7939904ada090eab2f",
+    "url": "/react-tedy-shopping-with-redux/static/media/soda.9e8968b6.jpg"
+  },
+  {
+    "revision": "9c31209caae2e4ba963672d8ddef1465",
+    "url": "/react-tedy-shopping-with-redux/static/media/tomato-juice.9c31209c.jpg"
+  }
+]);
